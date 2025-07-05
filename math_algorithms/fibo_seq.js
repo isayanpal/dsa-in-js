58ac363ef7e82cd44821eb8a69ce1c11a0e0d54d
Sayan Snigdha Pal <sayanpalb@gmail.com>
2025-07-05 21:37:34 +0530

    fibonacci sequence

---
+++ math_algorithms/fibo_seq.js
@@ -0,0 +1,20 @@
+// Give a number 'n', find the  first 'n' elements of the Fibonacci sequence
+// In maths, the fibonacci sequence is sequence in which each number is the sum of the two preceding ones.
+// The first two numbers in the sequence are 0 and 1.
+
+// fibonacci(2) = [0,1]
+// fibonacci(3) = [0,1,1]
+
+function fibonacci(n){
+    const fib = [0,1];
+    for(let i = 2; i <n; i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib;
+}
+
+console.log(fibonacci(2), "fibo-2")
+console.log(fibonacci(3), "fibo-3")
+console.log(fibonacci(7), "fibo-7")
+
+// Big-O of this algorithm is O(n) - reason : 1 for loop
